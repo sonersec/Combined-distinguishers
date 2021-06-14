50f6e7ff6c9c103fa2d6e484602997801793e88c
sonersec <75815279+sonersec@users.noreply.github.com>
2021-06-14 19:54:25 -0400

    Add files via upload

---
+++ cpa/HamWt.m
@@ -0,0 +1,6 @@
+%% Task: return Hamming weight of input vector 'v'
+function w=HamWt(v)
+% this function takes in a vector v which is a binary number and returns
+% the hamming weight of that
+w=sum(v);
+end
